c12b560193fb7a422dbcc08a106a3f22ddbb6adc
Pochiwas <Pochiwas@gmail.com>
2023-07-25 13:08:31 -0400

    add loader to app

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -4,10 +4,11 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomePageComponent, SearchBoxComponent, CardListComponent, CardsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule,SharedModule],
   exports: [HomePageComponent, SearchBoxComponent,CardListComponent,CardsComponent],
 })
 export class GifsModule {}
